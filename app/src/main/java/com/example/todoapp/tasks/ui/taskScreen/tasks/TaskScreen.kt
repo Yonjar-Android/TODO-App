052d95d9385eff6fbef7dac4312b5ba44a49fdeb
Yonjar <juan132y@gmail.com>
2024-07-26 15:25:48 -0600

    Se ha realizado el diseño del checkBox de las tareas para ser cargadas en el LazyColumn

--- app/src/main/java/com/example/todoapp/tasks/ui/taskScreen/tasks/TaskScreen.kt
+++ app/src/main/java/com/example/todoapp/tasks/ui/taskScreen/tasks/TaskScreen.kt
@@ -9,19 +9,24 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.DatePicker
 import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.DropdownMenuItem
@@ -45,10 +50,12 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
@@ -97,10 +104,14 @@ fun TaskScreen(
         Scaffold(
             content = {
                 BackgroundScreen(image = R.drawable.main_bg) {
-                    LazyColumn(modifier = Modifier.padding(it.calculateTopPadding())) {
-                        if (fetchedTasks.value != null){
-                            items(fetchedTasks.value!!){ task ->
-                                Text(text = task.name)
+                    LazyColumn(
+                        modifier = Modifier.fillMaxSize()
+                            .padding(horizontal = 8.dp),
+                        contentPadding = it
+                    ) {
+                        if (fetchedTasks.value != null) {
+                            items(fetchedTasks.value!!) { task ->
+                                TaskCheckBox(task = task)
                             }
                         }
                     }
@@ -399,6 +410,36 @@ fun DropDowsMenuCategories(
             }
         }
     }
+}
+
+@Composable
+fun TaskCheckBox(task: TaskDom) {
+    Spacer(modifier = Modifier.size(8.dp))
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.White)
+            .padding(4.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        val checkedState = rememberSaveable { mutableStateOf(task.check) }
+
+        Text(
+            text = task.name,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            modifier = Modifier.weight(9f),
+            fontSize = 16.sp
+        )
+        Checkbox(
+            checked = checkedState.value,
+            onCheckedChange = { checkedState.value = it },
+            modifier = Modifier
+                .weight(1f)
+                .clip(CircleShape)
+        )
+    }
+    Spacer(modifier = Modifier.size(8.dp))
 }
 
 
